be64f68806b135e628801ec7366f64bb1dedc91e
Ubuntu <ubuntu@ip-172-31-0-69.ec2.internal>
2016-06-13 22:24:58 +0000

    revising toggle between exact and approx

--- master_files/src/main/scala/site_data.scala
+++ master_files/src/main/scala/site_data.scala
@@ -30,7 +30,7 @@ object DataStreaming {
     val topicsSet = topics.split(",").toSet
 
     // configure batch intervals and create context
-    val interval = 5  // seconds
+    val interval = 1  // seconds
     val buildname = "site_data"  // name of build file
     val sparkConf = new SparkConf().setAppName(buildname)
     val ssc = new StreamingContext(sparkConf, Seconds(interval))
@@ -45,9 +45,16 @@ object DataStreaming {
     var all_males = globalHll.zero  // HLL counter for all males
     var all_age2 = globalHll.zero  // HLL counter for all users in age seg
 
-    // initialize counters to store counted elements 
-    // var userSet: Set[Int] = Set()
-
+    // initialize counters to store set of counted elements
+    // note this implementation assumes we can convert user ID to integer 
+    var userSet: Set[Int] = Set()
+    var malesSet: Set[Int] = Set()
+    var age2Set: Set[Int] = Set()
+ 
+    // control what to output
+    val approxcounts = true
+    val exactcounts = true 
+  
     // process each data record 
     messages.foreachRDD { rdd =>
 
@@ -75,56 +82,96 @@ object DataStreaming {
         val age2_filter = ticks_pre.filter (x => x._2._1 == "18-34")
         val age2 = age2_filter.keys
 
-        //  1. count ids
-        val approxids_pre = ids.mapPartitions(ids => {
+        if (approxcounts){
+            //  1. count ids
+            val approxids_pre = ids.mapPartitions(ids => {
                                     val hll = new HyperLogLogMonoid(12)
                                     ids.map(id => hll(id))
                                      })
 
-        if (approxids_pre.count()!=0){
-            val approxids_hll = approxids_pre.reduce(_ + _)
-            all_ids += approxids_hll  // update global
-            println("OUT: Approx distinct ids this batch: %d".format(approxids_hll.estimatedSize.toInt))
-            println("OUT: Approx distinct ids overall: %d".format(globalHll.estimateSize(all_ids).toInt))
-        }else{
-        println("OUT: Approx distinct ids this batch: 0")
-        println("OUT: Approx distinct ids overall: %d".format(globalHll.estimateSize(all_ids).toInt))
+            if (approxids_pre.count()!=0){
+                val approxids_hll = approxids_pre.reduce(_ + _)
+                all_ids += approxids_hll  // update global
+                println("OUT: Approx distinct ids this batch: %d".format(approxids_hll.estimatedSize.toInt))
+                println("OUT: Approx distinct ids overall: %d".format(globalHll.estimateSize(all_ids).toInt))
+            }else{
+                println("OUT: Approx distinct ids this batch: 0")
+                println("OUT: Approx distinct ids overall: %d".format(globalHll.estimateSize(all_ids).toInt))
+            }
+
+            // 2. count males
+            val approxmales_pre = males.mapPartitions(ids => {
+                                    val hll = new HyperLogLogMonoid(12)
+                                    ids.map(id => hll(id))
+                                     })
+
+            if (approxmales_pre.count()!=0){
+                val approxmales_hll = approxmales_pre.reduce(_ + _)
+                all_males += approxmales_hll  // update global
+                println("OUT: Approx distinct males this batch: %d".format(approxmales_hll.estimatedSize.toInt))
+                println("OUT: Approx distinct males overall: %d".format(globalHll.estimateSize(all_males).toInt))
+            }else{
+                println("OUT: Approx distinct males this batch: 0")
+                println("OUT: Approx distinct males overall: %d".format(globalHll.estimateSize(all_males).toInt))
+            }                               
+
+            // 3. count ids belonging to specific group            
+            val approxage2_pre = age2.mapPartitions(ids => {
+                                    val hll = new HyperLogLogMonoid(12)
+                                    ids.map(id => hll(id))
+                                     })
+
+            if (approxage2_pre.count()!=0){
+                val approxage2_hll = approxage2_pre.reduce(_ + _)
+                all_age2 += approxage2_hll  // update global
+                println("OUT: Approx distinct 18-34s this batch: %d".format(approxage2_hll.estimatedSize.toInt))
+                println("OUT: Approx distinct 18-34s overall: %d".format(globalHll.estimateSize(all_age2).toInt))
+            }else{
+                println("OUT: Approx distinct 18-34s this batch: 0")
+                println("OUT: Approx distinct 18-34s overall: %d".format(globalHll.estimateSize(all_age2).toInt))
+            }
+
         }
 
-        // 2. count males
-        val approxmales_pre = males.mapPartitions(ids => {
-                                    val hll = new HyperLogLogMonoid(12)
-                                    ids.map(id => hll(id))
-                                     })
 
-        if (approxmales_pre.count()!=0){
-            val approxmales_hll = approxmales_pre.reduce(_ + _)
-            all_males += approxmales_hll  // update global
-            println("OUT: Approx distinct males this batch: %d".format(approxmales_hll.estimatedSize.toInt))
-            println("OUT: Approx distinct males overall: %d".format(globalHll.estimateSize(all_males).toInt))
-        }else{
-        println("OUT: Approx distinct males this batch: 0")
-        println("OUT: Approx distinct males overall: %d".format(globalHll.estimateSize(all_males).toInt))
-        }                               
+        // calculate exact counts if desired, using sets
+        if (exactcounts){
+            //  1. count ids
+            if (ids.count()!=0){
+                val totalids = ids.map(id => Set(id)).reduce(_ ++ _)
+                userSet += totalids
+                println("OUT: Exact distinct ids this batch: %d".format(totalids.size)) 
+                println("OUT: Exact distinct ids overall: %d".format(userSet.size))
+            }else{
+                println("OUT: Exact distinct ids this batch: 0")
+                println("OUT: Exact distinct ids overall: %d".format(userSet.size))
+            }
 
-        // 3. count ids belonging to specific group            
-        val approxage2_pre = age2.mapPartitions(ids => {
-                                    val hll = new HyperLogLogMonoid(12)
-                                    ids.map(id => hll(id))
-                                     })
+            //  2. count males
+            if (males.count()!=0){
+                val totalmales = males.map(id => Set(id)).reduce(_ ++ _)
+                malesSet += totalmales
+                println("OUT: Exact distinct males this batch: %d".format(totalmales.size))
+                println("OUT: Exact distinct males overall: %d".format(malesSet.size))
+            }else{
+                println("OUT: Exact distinct males this batch: 0")
+                println("OUT: Exact distinct males overall: %d".format(malesSet.size))
+            }
 
-        if (approxage2_pre.count()!=0){
-            val approxage2_hll = approxage2_pre.reduce(_ + _)
-            all_age2 += approxage2_hll  // update global
-            println("OUT: Approx distinct 18-34s this batch: %d".format(approxage2_hll.estimatedSize.toInt))
-            println("OUT: Approx distinct 18-34s overall: %d".format(globalHll.estimateSize(all_age2).toInt))
-        }else{
-        println("OUT: Approx distinct 18-34s this batch: 0")
-        println("OUT: Approx distinct 18-34s overall: %d".format(globalHll.estimateSize(all_age2).toInt))
+            // 3. count ids belonging to specific group
+            if (age2.count()!=0){ 
+                val totalage2 = age2.map(id => Set(id)).reduce(_ ++ _)
+                age2Set += totalage2
+                println("OUT: Exact distinct 18-34s this batch: %d".format(totalage2.size))
+                println("OUT: Exact distinct 18-34s overall: %d".format(age2Set.size))
+            }else{
+                println("OUT: Exact distinct 18-34s this batch: 0")
+                println("OUT: Exact distinct 18-34s overall: %d".format(age2Set.size))
+            }
+
         }
 
     }
-
     // Start the computation
     ssc.start()
     ssc.awaitTermination()
